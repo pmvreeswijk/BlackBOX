81dd87b7dd4222bcfd01027f9e24202e8d42feaa
Paul Vreeswijk <pmvreeswijk@gmail.com>
2019-02-27 19:20:48 -0300

    updated startup script

--- startup_blackbox_zogy.sh
+++ startup_blackbox_zogy.sh
@@ -1,76 +1,82 @@
 #!/bin/bash
 
-# This is a bash script to help install BlackBOX and ZOGY
-# automatically on a linux machine. It has been tested using a Google
-# cloud VM instance with a fresh install of Ubuntu 18.04 LTS, and runs
-# mostly fine.
+# Bash script to help install BlackBOX and ZOGY automatically on a
+# linux machine. It has been tested using a Google cloud VM instance
+# with a fresh install of Ubuntu 18.04 LTS.
+#
+# to run: download and execute "sudo ./startup_blackbox_zogy.sh"
 #
 # Still to do:
 #
 # - add the calibration binary fits catalog used by zogy
 # - add the Astrometry.net index files
-# - try to install PSFEx (and SExtractor) with multi-threading
+# - try to make apt-get install PSFEx (and SExtractor) with multi-threading
 
+# versions/settings
+# -----------------
+
+# python version
+v_python="3.7"
+# zogy and blackbox versions; leave these empty ("") or comment out
+# for latest versions
+v_blackbox="0.8"
+v_zogy="0.8"
+
+# define home of zogy, data and blackbox
+zogyhome=${PWD}/ZOGY
+datahome=${PWD}
+blackboxhome=${PWD}/BlackBOX
 
 # ubuntu update/upgrade
 # ---------------------
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get -y update
+apt-get -y upgrade
 
 
-# python 2 or 3
-# -------------
-# meant to use python3.7, but this is giving problems in Ubuntu 18.04
-# with pip being 3.6, so for the moment stick to python3 (whichs
-# points to python3.6)
-version=3
-if (( $(echo "$version < 3.0" |bc -l) )); then
-    sudo apt-get -y install python2.7 python2.7-dev
-    echo alias python=python2.7 >> ${HOME}/.bash_aliases
-    pip=pip;
+# python and pip
+# --------------
+apt-get -y install python${v_python} python${v_python}-dev
+if [ ${v_python} \< "3" ]
+then
+    apt-get -y install python-pip
 else
-    sudo apt-get -y install python3 python3-dev
-    echo alias python=python3 >> ${HOME}/.bash_aliases
-    pip=/usr/bin/pip3;
-    echo alias pip=${pip} >> ${HOME}/.bash_aliases
+    apt-get -y install python3-pip
 fi
-    
+pip="python${v_python} -m pip"
+
 
 # git (already installed in non-minimal version of 18.04)
 # -------------------------------------------------------
-sudo apt-get -y install git
+apt-get -y install git
 
 
-# clone ZOGY and BlackBOX repositories
-# ------------------------------------
-cd $HOME
-git clone https://github.com/pmvreeswijk/ZOGY
-git clone https://github.com/pmvreeswijk/BlackBOX
-
-
-# define environent variables:
-# ----------------------------
-zogyhome=${HOME}/ZOGY
-# for the moment add these to .bashrc such that
-# next login they are available
-echo export ZOGYHOME=${zogyhome} >> ${HOME}/.bashrc
-echo export DATAHOME=${HOME} >> ${HOME}/.bashrc
+# clone ZOGY and BlackBOX repositories in current directory
+# ---------------------------------------------------------
+if [ ! -z ${v_zogy} ]
+then
+    zogy_branch="--branch v${v_zogy}"
+    v_zogy_git="@v${v_zogy}"
+fi
+git clone ${zogy_branch} https://github.com/pmvreeswijk/ZOGY
+if [ ! -z ${v_blackbox} ]
+then
+    blackbox_branch="--branch v${v_blackbox}"
+    v_blackbox_git="@v${v_blackbox}"
+fi
+git clone ${blackbox_branch} https://github.com/pmvreeswijk/BlackBOX
 
 
 # rsync BlackBOX content to ZOGY
 # ------------------------------
-rsync -av ${HOME}/BlackBOX/* ${zogyhome}
-
-
-# update PYTHONPATH
-# -----------------
-echo export PYTHONPATH=${zogyhome}\:${zogyhome}/Settings >> ${HOME}/.bashrc
+rsync -av ${blackboxhome}/* ${zogyhome}
+# remove BlackBOX to avoid confusion
+rm -rf ${blackboxhome}
 
 
 # install ZOGY and BlackBOX repositories
 # --------------------------------------
-sudo ${pip} install git+git://github.com/pmvreeswijk/ZOGY
-sudo ${pip} install git+git://github.com/pmvreeswijk/BlackBOX
+${pip} install git+git://github.com/pmvreeswijk/ZOGY${v_zogy_git}
+${pip} install git+git://github.com/pmvreeswijk/BlackBOX${v_blackbox_git}
 
 
 # download calibration catalog
@@ -82,29 +88,67 @@ sudo ${pip} install git+git://github.com/pmvreeswijk/BlackBOX
 # ---------------------
 
 # Astrometry.net
-sudo apt-get -y install astrometry.net
+apt-get -y install astrometry.net
 # make sure correct Index files are saved in correct directory on
 # mlcontrol these are in /usr/local/astrometry/data/ but this
 # directory does not exist on GCloud VM it is /usr/share/astrometry/,
 # while config file is at /etc/astrometry.cfg
 
 # SExtractor (although it seems already installed automatically)
-sudo apt-get -y install sextractor
+apt-get -y install sextractor
 # the executable for this installation is 'sextractor' while ZOGY
 # expects 'sex'; make a symbolic link
-sudo ln -s /usr/bin/sextractor /usr/bin/sex
+ln -s /usr/bin/sextractor /usr/bin/sex
 
 # SWarp
-sudo apt-get -y install swarp
+apt-get -y install swarp
 # the executable for this installation is 'SWarp' while ZOGY expects
 # 'swarp'; make a symbolic link
-sudo ln -s /usr/bin/SWarp /usr/bin/swarp
+ln -s /usr/bin/SWarp /usr/bin/swarp
 
 # PSFEx - this basic install does not allow multi-threading
-sudo apt-get -y install psfex
+apt-get -y install psfex
 
 # ds9
-sudo apt-get -y install saods9
+apt-get -y install saods9
 
 # dfits
-sudo apt-get -y install qfits-tools
+apt-get -y install qfits-tools
+
+
+# set environent variables:
+# -------------------------
+if [[ ${SHELL} == *"bash"* ]]
+then
+    export ZOGYHOME=${zogyhome}
+    export DATAHOME=${datahome}
+    if [ -z "$PYTHONPATH" ]
+    then
+	export PYTHONPATH=${zogyhome}\:${zogyhome}/Settings
+    else
+	export PYTHONPATH=${PYTHONPATH}\:${zogyhome}\:${zogyhome}/Settings
+    fi
+elif [[ ${SHELL} == *"csh"* ]]
+then
+    setenv ZOGYHOME ${zogyhome}
+    setenv DATAHOME ${datahome}
+    if [ -z "$PYTHONPATH" ]
+    then
+	setenv PYTHONPATH ${zogyhome}\:${zogyhome}/Settings
+    else
+	setenv PYTHONPATH ${PYTHONPATH}\:${zogyhome}\:${zogyhome}/Settings
+    fi
+fi
+echo
+echo "----------------------------------------------------------------------"
+echo "add these commands to your shell startup script (~/.bashrc, ~/.cshrc"
+echo "or ~/.zshrc) for these variables to be set next login, e.g. for bash:"
+echo 
+echo "export ZOGYHOME=${zogyhome}"
+echo "export DATAHOME=${datahome}"
+echo "export PYTHONPATH=\$PYTHONPATH:${zogyhome}:${zogyhome}/Settings"
+echo
+echo "and add an alias for python:"
+echo
+echo "alias python=python${v_python}"
+echo
