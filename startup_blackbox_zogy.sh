41c6635ce8841d70d016509c85b241e41f8be49a
Paul Vreeswijk <pmvreeswijk@gmail.com>
2019-03-06 17:47:07 -0300

    updated startup script

--- startup_blackbox_zogy.sh
+++ startup_blackbox_zogy.sh
@@ -13,11 +13,11 @@
 
 
 # versions/settings
-# -----------------
+# ================================================================================
+
 # python version
 v_python="3.7"
-# zogy and blackbox versions; leave these empty ("") or comment out
-# for latest versions
+# zogy and blackbox; for latest version, leave these empty ("") or comment out
 v_blackbox="0.8"
 v_zogy="0.8"
 
@@ -26,31 +26,44 @@ zogyhome=${PWD}/ZOGY
 datahome=${PWD}
 blackboxhome=${PWD}/BlackBOX
 
-# ubuntu update/upgrade
-# ---------------------
-sudo apt-get -y update
-sudo apt-get -y upgrade
+
+# check Linux version, update/upgrade it and set package manager
+# ================================================================================
+
+uname_str=$(uname -a)
+echo ${uname_str}
+if [[ ${uname_str,,} == *"ubuntu"* ]]
+then
+    packman="apt-get"
+    # update
+    sudo ${packman} -y update
+#elif [[ ${uname_str,,} == *"fedora"* ]]
+#then
+#    packman="dnf"
+fi
+# upgrade
+sudo ${packman} -y upgrade
 
 
-# python and pip
-# --------------
-sudo apt-get -y install python${v_python} python${v_python}-dev
+# python, pip and git
+# ================================================================================
+
+sudo ${packman} -y install python${v_python} python${v_python}-dev
 if [ ${v_python} \< "3" ]
 then
-    sudo apt-get -y install python-pip
+    sudo ${packman} -y install python-pip
 else
-    sudo apt-get -y install python3-pip
+    sudo ${packman} -y install python3-pip
 fi
 pip="python${v_python} -m pip"
 
-
-# git (already installed in non-minimal version of 18.04)
-# -------------------------------------------------------
-sudo apt-get -y install git
+# git
+sudo ${packman} -y install git
 
 
 # clone ZOGY and BlackBOX repositories in current directory
-# ---------------------------------------------------------
+# ================================================================================
+
 if [ ! -z ${v_zogy} ]
 then
     zogy_branch="--branch v${v_zogy}"
@@ -65,51 +78,51 @@ then
 fi
 git clone ${blackbox_branch} https://github.com/pmvreeswijk/BlackBOX
 
-
 # rsync BlackBOX content to ZOGY
-# ------------------------------
 rsync -av ${blackboxhome}/* ${zogyhome}
 # remove BlackBOX to avoid confusion
 rm -rf ${blackboxhome}
 
 
 # install ZOGY and BlackBOX repositories
-# --------------------------------------
+# ================================================================================
+
 sudo -H ${pip} install git+git://github.com/pmvreeswijk/ZOGY${v_zogy_git}
 sudo -H ${pip} install git+git://github.com/pmvreeswijk/BlackBOX${v_blackbox_git}
 
 
 # download calibration catalog
-# ----------------------------
+# ================================================================================
+
 # From where? To be put in ${ZOGYHOME}/CalFiles/
 
 
 # packages used by ZOGY
-# ---------------------
+# ================================================================================
 
 # Astrometry.net
-sudo apt-get -y install astrometry.net
+sudo ${packman} -y install astrometry.net
 
 # SExtractor (although it seems already installed automatically)
-sudo apt-get -y install sextractor
+sudo ${packman} -y install sextractor
 # the executable for this installation is 'sextractor' while ZOGY
 # expects 'sex'; make a symbolic link
 sudo ln -s /usr/bin/sextractor /usr/bin/sex
 
 # SWarp
-sudo apt-get -y install swarp
+sudo ${packman} -y install swarp
 # the executable for this installation is 'SWarp' while ZOGY expects
 # 'swarp'; make a symbolic link
 sudo ln -s /usr/bin/SWarp /usr/bin/swarp
 
 # PSFEx - this basic install does not allow multi-threading
-sudo apt-get -y install psfex
+sudo ${packman} -y install psfex
 
 # ds9
-sudo apt-get -y install saods9
+sudo ${packman} -y install saods9
 
 # dfits
-sudo apt-get -y install qfits-tools
+sudo ${packman} -y install qfits-tools
 
 
 # set environent variables:
@@ -158,12 +171,15 @@ echo
 echo "======================================================================"
 echo
 
+
 # use Astrometry.net link to DR2 files
 url="http://data.astrometry.net/5000"
-# make sure correct Index files are saved in correct directory on
-# mlcontrol these are in /usr/local/astrometry/data/ but this
-# directory does not exist on GCloud VM; there it is
-# /usr/share/astrometry/, while config file is at /etc/astrometry.cfg
+
+# make sure index files are saved in the right directory; on mlcontrol
+# these are in /usr/local/astrometry/data/ (config file:
+# /usr/local/astrometry/etc/astrometry.cfg) while on GCloud VM
+# installation they are supposed to be in /usr/share/astrometry
+# (config file: /etc/astrometry.cfg)
 dir1="/usr/share/astrometry"
 dir2="/usr/local/astrometry/data"
 dir3="${HOME}/IndexFiles"
@@ -181,3 +197,4 @@ echo "downloading Astrometry.net index files to directory ${dir_save}"
 echo 
 sudo wget -nc $url/index-500{5..6}-0{0..9}.fits -P ${dir_save}
 sudo wget -nc $url/index-500{5..6}-1{0..1}.fits -P ${dir_save}
+
