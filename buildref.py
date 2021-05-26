4560008fb9f951e8c6437b11e1b32db4f6c120dd
Paul Vreeswijk <pmvreeswijk@gmail.com>
2021-05-26 17:24:08 +0200

    added [str2bool] to try to avoid error message related to it when testing the singularity container

--- buildref.py
+++ buildref.py
@@ -3426,6 +3426,20 @@ def run_coaddition(M, P, sigma, F, nimages):
 
 ################################################################################
 
+# from https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+################################################################################
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='build MeerLICHT/BlackGEM '
