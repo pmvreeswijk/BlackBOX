74b2dfec8c2bfef8008766cdbadb14978bf5eb8b
Paul Vreeswijk <pmvreeswijk@gmail.com>
2025-03-06 16:23:05 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='blackbox',
-    version='1.3.4',
+    version='1.4.3',
     description='image processing sofware specifically written for the reduction of BlackGEM and MeerLICHT images',
     url='https://github.com/pmvreeswijk/BlackBOX',
     author='Paul Vreeswijk, Kerry Paterson, Danielle Pieterse',
